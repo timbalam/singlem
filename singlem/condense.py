343962853c926d40decf1818ae55c2bc9edd5ebc
Tim Lamberton <tim.lamberton@gmail.com>
2026-04-22 17:18:06 +1000

    implement new nmf core algorithm

--- singlem/condense.py
+++ singlem/condense.py
@@ -187,6 +187,8 @@ class Condenser:
             logging.info(f"Using coverage penalties {coverage_rank_penalty}")
             logging.info("Converting DIAMOND IDs to taxons")
             self._convert_diamond_best_hit_ids_to_taxonomies(metapackage, sample_otus)
+            logging.info("Demultiplexing OTU best hits")
+            self._demultiplex_best_hits_in_place(sample_otus)
             condensed_otus, coverage_parts_otus, loss = self._apply_nonneg_matrix_factorisation(
                 sample,
                 sample_otus,
@@ -638,9 +640,6 @@ class Condenser:
 
         logging.debug("Total coverage by query: {}".format(sum([o.coverage for o in sample_otus if o.taxonomy_assignment_method() == QUERY_BASED_ASSIGNMENT_METHOD])))
         logging.debug("Total coverage by diamond: {}".format(sum([o.coverage for o in sample_otus if o.taxonomy_assignment_method() == DIAMOND_ASSIGNMENT_METHOD])))
-        
-        logging.info("Demultiplexing OTU best hits")
-        self._demultiplex_best_hits_in_place(sample_otus)
 
         taxon_to_coverage, coverage_parts_otus, loss = self._apply_nonneg_matrix_factorisation_core(sample_otus, **kwargs)
 
@@ -712,6 +711,212 @@ class Condenser:
         return CondensedCommunityProfile(sample, sample_summary_root_node)
     
     def _apply_nonneg_matrix_factorisation_core(self, sample_otus, *,
+                                                genes_per_domain,
+                                                trim_percent = 0,
+                                                coverage_rank_penalty = None,
+                                                min_scale_factor = 1e-3,
+                                                mask_otus = None,
+                                                max_num_steps = None):
+        
+        # Set up initial conditions.
+        # taxon -> (cov_v, marker -> n -> prev_v)
+        taxon_to_params = {}
+        residues = []
+        masks = []
+        for (i, otu) in enumerate(sample_otus):
+            mask = mask_otus is not None and otu.sequence in mask_otus
+            residue = otu.coverage
+            if (otu.taxonomy_assignment_method()
+                in (QUERY_BASED_ASSIGNMENT_METHOD, DIAMOND_ASSIGNMENT_METHOD)):
+                marker = otu.marker
+                best_hit_taxonomies = otu.equal_best_hit_taxonomies()
+                good_taxonomies = otu.good_taxonomies()
+                for taxonomies in (best_hit_taxonomies, good_taxonomies):
+                    if taxonomies is not None:
+                        for best_hit_tax in taxonomies:
+                            clean_tax = TaxonomyUtils.clean_taxonomy_string(best_hit_tax)
+
+                            if clean_tax == 'Root':
+                                continue
+
+                            try:
+                                (_, _, gene_to_otu_to_prev) = taxon_to_params[clean_tax]
+                            except KeyError:
+                                gene_to_otu_to_prev = {}
+                                taxon_to_params[clean_tax] = (1, gene_to_otu_to_prev)
+                            try:
+                                otu_to_prev = gene_to_otu_to_prev[marker]
+                            except KeyError:
+                                otu_to_prev = {}
+                                gene_to_otu_to_prev[marker] = otu_to_prev
+                            
+                            if not mask:
+                                residue -= 1
+                            otu_to_prev[i] = 1
+            residues.push(residue)
+            if mask:
+                masks.push(i)
+
+        if mask_otus is not None:
+            if len(masks) == 0:
+                logging.warning("Masking 0 OTUs for this sample")
+            else:
+                logging.info(f"Masking {len(masks)} OTUs for this sample")
+
+        if len(taxon_to_params) == 0: return None, sample_otus
+        
+        num_steps = 1
+        min_num_steps = 50 if max_num_steps is None else min(50, max_num_steps)
+        delta = 1e-4
+        while True: # while not converged
+
+            # Pass over taxa
+            max_coef_change = 0
+            max_change_desc = ""
+            max_change_current = np.nan
+            max_change_update = np.nan
+            for tax, (coverage, gene_to_otu_to_prev) in taxon_to_params.items():
+                # Update residues
+                for otu_to_prev in gene_to_otu_to_prev.values():
+                    for i, prev in otu_to_prev.items():
+                        residues[i] += coverage * prev
+                # Algorithm 1 from Taslaman 2012
+                gene_to_otu_to_next_prev = {}
+                prev_sq_sum = 0
+                prev_residue_sum = 0
+                for marker, otu_to_prev in gene_to_otu_to_prev.items():
+                    consts = [
+                        ((residues[i] * coverage + delta * prev) / (current_coverage ** 2 + delta), i)
+                        for i, prev in otu_to_prev.items()
+                    ].sort()
+                    alpha = 1
+                    for (j, const_j) in enumerate(consts):
+                        alpha -= const_j
+                        min_pd = alpha / j
+                        if (j == len(consts)): break
+                        if (min_pd <= -consts[j+1]): break
+                    
+                    otu_to_next_prev = {}
+                    for const, i in consts[:j]:
+                        next_prev = const + min_pd
+                        otu_to_next_prev[i] = next_prev
+                        prev_sq_sum += next_prev ** 2
+                        prev_residue_sum += next_prev * residue[i]
+                        
+                        coef_change = abs(next_prev - prev)
+
+                        if coef_change > max_coef_change:
+                            max_coef_change = coef_change
+                            max_change_desc = f"{tax} OTU {i} prevalence"
+                            max_change_current = prev
+                            max_change_update = next_prev
+                    
+                    for _, i in consts[j:]:
+                        otu_to_next_prev[i] = 0
+                    
+                    gene_to_otu_to_next_prev[marker] = otu_to_next_prev
+                
+                next_coverage = max(
+                    prev_residue_sum - coverage_rank_penalty[TaxonomyUtils.rank(tax)] / 2,
+                    0
+                ) / prev_sq_sum
+
+                coef_change = abs(next_coverage - coverage)
+                if coef_change > max_coef_change:
+                    max_coef_change = coef_change
+                    max_change_desc = f"{tax} coverage"
+                    max_change_current = coverage
+                    max_change_update = next_coverage
+                    
+                for otu_to_next_prev in gene_to_otu_to_next_prev.values():
+                    for i, next_prev in otu_to_next_prev.items():
+                        residues[i] -= next_prev * next_coverage
+
+            need_another_iteration = num_steps < min_num_steps or max_coef_change > 0.001
+            if not need_another_iteration:
+                logging.info(f"NMF converged")
+                break
+
+            if num_steps % 100 == 0:
+                logging.info(f"Max coef change after {num_steps} steps {max_coef_change}")
+                logging.debug(f"{max_change_desc} changed from {max_change_current} to {max_change_update}")
+
+            num_steps += 1
+            
+            if max_num_steps is not None and num_steps > max_num_steps:
+                logging.warning(f"NMF failed to converge after max steps")
+                break
+        
+        # Round each genome to 4 decimal places in coverage, removing entries with 0 coverage
+        # Use 3 decimals to avoid rounding to 0 when one OTU is split between many species
+        rounded_taxon_to_coverage = {}
+        reg_loss = 0
+        for tax, (coverage, _) in taxon_to_params.items():
+            cov2 = round(coverage, 3)
+            if cov2 > 0:
+                rounded_taxon_to_coverage[tax] = cov2
+            if coverage_rank_penalty is not None:
+                reg_loss += coverage_rank_penalty[TaxonomyUtils.rank(tax)] * coverage
+
+        coverage_parts_otus = ArchiveOtuTable()
+        coverage_parts_otus.fields = sample_otus.fields
+        loss = 0
+        mask_loss = 0
+        for i, (residue, otu) in enumerate(zip(residues, sample_otus)):
+            expected_coverage = residue - otu.coverage
+            
+            if i in masks:
+                mask_loss += residue ** 2
+            else:
+                loss += residue ** 2
+
+            if (expected_coverage == 0
+                or (otu.taxonomy_assignment_method()
+                    not in (QUERY_BASED_ASSIGNMENT_METHOD, DIAMOND_ASSIGNMENT_METHOD))):
+                new_otu = ArchiveOtuTableEntry()
+                new_otu.data = otu.data.copy()
+                new_otu.data[ArchiveOtuTable.TAXONOMY_FIELD_INDEX] = "Root"
+                new_otu.data[ArchiveOtuTable.COVERAGE_FIELD_INDEX] = otu.coverage
+                logging.debug(f"Adding OTU taxonomy {new_otu.taxonomy} with coverage {new_otu.coverage}")
+                coverage_parts_otus.add([new_otu])
+            else:
+                best_hit_taxonomies = otu.equal_best_hit_taxonomies()
+                good_taxonomies = otu.good_taxonomies()
+                for taxonomies in (best_hit_taxonomies, good_taxonomies):
+                    if taxonomies is not None:
+                        for best_hit_tax in taxonomies:
+                            clean_tax = TaxonomyUtils.clean_taxonomy_string(best_hit_tax)
+
+                            if clean_tax == 'Root':
+                                continue
+
+                            (coverage, gene_to_otu_to_prev) = taxon_to_params[clean_tax]
+                            prev = gene_to_otu_to_prev[otu.marker][i]
+                    
+                            #part = round(next_prev * next_coverage, 3)
+                            part = round(prev * coverage * otu.coverage / expected_coverage, 3)
+                            if part > 0:
+                                new_otu = ArchiveOtuTableEntry()
+                                new_otu.data = otu.data.copy()
+                                new_otu.data[ArchiveOtuTable.TAXONOMY_FIELD_INDEX] = clean_tax
+                                new_otu.data[ArchiveOtuTable.COVERAGE_FIELD_INDEX] = part
+                                logging.debug(f"Adding OTU taxonomy {new_otu.taxonomy} with coverage {new_otu.coverage}")
+                                coverage_parts_otus.add([new_otu])
+        
+        loss_dict = {}
+        loss_dict['NMF loss'] = round(loss, 3)
+        if mask_otus is not None:
+            loss_dict['NMF mask loss'] = round(mask_loss, 3)
+        if coverage_rank_penalty is not None:
+            loss_dict['NMF penalised loss'] = round(loss + reg_loss, 3)
+        loss_dict['NMF steps'] = num_steps
+        
+        for key, value in loss_dict.items():
+            logging.info(f"{key} {value}")
+
+        return rounded_taxon_to_coverage, coverage_parts_otus, loss_dict
+
+    def _apply_nonneg_matrix_factorisation_old_core(self, sample_otus, *,
                                                 genes_per_domain,
                                                 trim_percent = 0,
                                                 coverage_rank_penalty = None,
